f7d0553a4f65ab1a62f7bf629cfd444268afa6d6
Sparky101 <33371779+Sparky101-2@users.noreply.github.com>
2025-08-21 14:17:45 +0000

    .

--- chrome/bookmarks-button/background.js
+++ chrome/bookmarks-button/background.js
@@ -1,11 +1,3 @@
 chrome.action.onClicked.addListener(() => {
-  chrome.bookmarks.search({ title: "Mobile bookmarks" }, (results) => {
-    if (results.length > 0) {
-      const mobileFolder = results[0];
-      chrome.tabs.create({ url: `chrome://bookmarks/?id=${mobileFolder.id}` });
-    } else {
-      // Fallback: open bookmarks manager if folder not found
-      chrome.tabs.create({ url: "chrome://bookmarks/" });
-    }
-  });
+  chrome.tabs.create({ url: "chrome://bookmarks/" });
 });
